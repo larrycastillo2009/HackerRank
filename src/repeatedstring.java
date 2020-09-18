0752b6126d52da6b513b00c01ffe88d6a0559d59
Larry Castillo <larrycastillo2009@gmail.com>
2020-09-18 14:34:52 -0500

    repo refract

--- src/repeatedstring.java
+++ src/repeatedstring.java
@@ -4,9 +4,8 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class repeatedstring {
-    // Complete the repeatedString function below.
-    static long repeatedString(String s, long n) {
-        if (s.equals("a")) return n:
+    static long repeatedStrings(String s, long n) {
+        if (s.equals("a")) return n;
         long count = countIt(s, s.length());
         long div = n / s.length();
         return (div * count) + countIt(s, n % s.length());
@@ -16,56 +15,18 @@ public class repeatedstring {
         long count = 0;
         for (int i=0; i<times;i ++){
             if (s.charAt(i) == 'a'){
-                count++
+                count++;
             }
-            return count;
         }
-//        String nstr = "";
-//        long counter = 0;
-//        if(s.equals("a")){
-//            return n;
-//        }else{
-//        for(int i = 0; i <= n; i++){
-//            if(nstr.length() + s.length() > n){
-//                String t ="";
-//                for (int i = 0; i < (n-nstr.length()); i++){
-//                    t += s.charAt(i);
-//                }
-//                nstr = nstr + t;
-//                nstr = nstr.substring(0,);
-//            }else{
-//                nstr = nstr + s;
-//            }
-//        }
-//
-//        for(int i = 0; i < nstr.length(); i++){
-//            if(nstr.charAt(i) == 'a'){
-//                counter++;
-//            }
-//        }
-//        return counter;
-//        }
-
+        return count;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-//
-//        String s = scanner.nextLine();
-//
-//        long n = scanner.nextLong();
-//        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-        System.out.println(repeatedString("a",10000000000));
-//        long result = repeatedString("a", 1000000000000);
-//
-//        bufferedWriter.write(String.valueOf(result));
-//        bufferedWriter.newLine();
-//
-//        bufferedWriter.close();
-//
-//        scanner.close();
+
+//        System.out.println(repeatedStrings("a",10000000000));
+
     }
 
 }
